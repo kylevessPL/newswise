dd7f6170618054dad335d0b1eff464fc24b9f4a9
Kacper Piasta <kacperpiasta@gmail.com>
2023-12-10 23:03:04 +0100

    polish OpenNLPHelper

--- newswise-backend/src/main/kotlin/pl/piasta/newswise/classification/opennlp/OpenNLPHelper.kt
+++ newswise-backend/src/main/kotlin/pl/piasta/newswise/classification/opennlp/OpenNLPHelper.kt
@@ -35,35 +35,4 @@ object OpenNLPHelper {
         }
         DocumentCategorizerME(doccat)
     }
-
-    fun softmax(logits: Collection<Double>): Collection<Double> {
-        val expLogits = logits.map { exp(it) }
-        val sumExp = expLogits.sum()
-        return expLogits.map { it / sumExp }
-    }
-
-    suspend fun l1NormalizeTFIDF(
-        datum: Datum<String, String>,
-        featureDocCounts: Counter<String>,
-        dispatcher: CoroutineDispatcher = Dispatchers.Default
-    ): RVFDatum<String, String> = withContext(dispatcher) {
-        Dataset<String, String>().apply { add(datum) }.getL1NormalizedTFIDFDatum(datum, featureDocCounts)
-    }
-
-    suspend fun selectFeaturesByThreshold(
-        datum: RVFDatum<String, String>,
-        featureWeightThreshold: Double,
-        dispatcher: CoroutineDispatcher = Dispatchers.Default
-    ) = withContext(dispatcher) {
-        datum.apply {
-            val maxWeight = asFeaturesCounter().values().max()
-            val threshold = calculateThreshold(featureWeightThreshold, maxWeight)
-            with(asFeaturesCounter()) {
-                entrySet().filter { it.value < threshold }.forEach { remove(it.key) }
-            }
-        }
-    }
-
-    private fun calculateThreshold(featureWeightThreshold: Double, maxWeight: Double) =
-        featureWeightThreshold.times(maxWeight).div(100)
 }
